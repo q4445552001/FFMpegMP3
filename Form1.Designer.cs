971d8385dd8f40f7c6a5be8f6afbda35827bff71
q4445552001 <42619163+q4445552001@users.noreply.github.com>
2019-06-04 17:50:09 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace FFMpeg_mp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            resources.ApplyResources(this.listBox1, "listBox1");
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            // 
+            // progressBar1
+            // 
+            resources.ApplyResources(this.progressBar1, "progressBar1");
+            this.progressBar1.Name = "progressBar1";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+
+namespace FFMpeg_mp3
+{
+    public partial class Form1 : Form
+    {
+        Thread mp3t0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //讀取檔案
+            OpenFileDialog openfile = new OpenFileDialog();
+            openfile.Multiselect = true;
+            openfile.Filter = "*|*";
+            if (openfile.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string file in openfile.FileNames)
+                {
+                    listBox1.AllowDrop = false;
+                    listBox1.Items.Add(file);
+                    listBox1.AllowDrop = true;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mp3t0 = new Thread(() =>
+            {
+                progressBar1.Value = 0;
+                double count = 0;
+                foreach (string list in listBox1.Items)
+                {
+                    textBox2.Text = list;
+                    textBox1.Text = (count + 1) + " / " + listBox1.Items.Count;
+                    Directory.SetCurrentDirectory(Path.GetDirectoryName(list));
+                    FileInfo batchItemAttribute = new FileInfo(list);
+                    batchItemAttribute.Attributes = FileAttributes.Normal;
+                    var run = new Process
+                    {
+                        StartInfo = new ProcessStartInfo
+                        {
+                            FileName = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase + @".\bin\ffmpeg\", "ffmpeg.exe"),
+                            Arguments = "-i \"" + list + "\" -codec:a libmp3lame -threads 4 -q:a 6 -y buff.mp3",
+                            UseShellExecute = false,
+                            CreateNoWindow = true,
+                            RedirectStandardError = true
+                        }
+                    };
+                    run.Start();
+                    StreamReader sr = run.StandardError;
+                    while (!sr.EndOfStream)
+                    {
+                        string srstring = sr.ReadLine();
+                        if (sr.EndOfStream)
+                        {
+                            File.Copy("buff.mp3", Path.GetFileNameWithoutExtension(list) + ".mp3", true);
+                            File.Delete("buff.mp3");
+                        }
+                    }
+                    count++;
+                    double prodata = Math.Round(count / listBox1.Items.Count * 100,2);
+                    progressBar1.Value = (int)prodata;
+                    label4.Text = prodata.ToString() + " %";
+                }   
+            });
+            mp3t0.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            while (true)
+            {
+                if (mp3t0 != null && mp3t0.IsAlive) mp3t0.Abort();
+                if (!mp3t0.IsAlive)
+                {
+                    foreach (Process p in Process.GetProcessesByName("ffmpeg")) p.Kill();
+                    File.Delete("buff.mp3");
+                    MessageBox.Show("以強制停止", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+                }
+            }
+        }
+
+    }
+}
